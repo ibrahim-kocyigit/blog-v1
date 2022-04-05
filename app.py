8d72e63f8485ffc4fc5427af0ff2d3278f0c1957
Ibrahim <ibrahimkocyigit@gmail.com>
2022-04-05 18:19:42 +0300

    Adds local db besides Heroku db

--- app.py
+++ app.py
@@ -19,7 +19,8 @@ app = Flask(__name__)
 load_dotenv()
 
 app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
+    "DATABASE_URL",  "sqlite:///blog_032622.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['MAIL_SERVER'] = os.getenv("MAIL_SERVER")
 app.config['MAIL_PORT'] = os.getenv("MAIL_PORT")
